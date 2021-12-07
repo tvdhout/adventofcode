a7b4c728b8c30425988d219d640f5e83d43807d2
tvdhout <thijsvandenhout@live.nl>
2021-12-07 07:57:49 +0100

    Day 7

--- 2021/7/pt2.py
+++ 2021/7/pt2.py
@@ -5,5 +5,5 @@ with open('input.txt') as f:
     positions = np.array(f.read().split(','), dtype=int)
 
 distance = abs(positions-int(np.mean(positions)))  # distance to the (rounded) mean: fewest average nr of steps
-fuel_cost = list(map(lambda d: math.comb(d+1, 2), distance))  # (distance+1 choose n) is the fuel cost
+fuel_cost = map(lambda d: math.comb(d+1, 2), distance)  # (distance+1 choose n) is the fuel cost
 print(sum(fuel_cost))
